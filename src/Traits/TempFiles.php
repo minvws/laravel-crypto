78267f6fbeaa0d28701d45d1295fc1ebd50429ec
Rick Lambrechts <rick@rl-webdiensten.nl>
2022-08-26 16:12:55 +0200

    Added trait to handle temp files

---
+++ src/Traits/TempFiles.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MinVWS\Crypto\Laravel\Traits;
+
+use MinVWS\Crypto\Laravel\Exceptions\FileException;
+
+trait TempFiles
+{
+    /**
+     * Creates a temp file with the supplied content and return the path of the file.
+     * @param string $content
+     * @return resource
+     */
+    public function createTempFileWithContent(string $content)
+    {
+        $tmpFile = $this->createTempFile();
+
+        $tmpFilePath = $this->getTempFilePath($tmpFile);
+        file_put_contents($tmpFilePath, $content);
+
+        return $tmpFile;
+    }
+
+    /**
+     * Get the file path of the resource.
+     * @param $resource
+     * @return string
+     */
+    protected function getTempFilePath($resource): string
+    {
+        if (!is_resource($resource)) {
+            throw FileException::variableIsNotAResource();
+        }
+
+        return stream_get_meta_data($resource)['uri'];
+    }
+
+    /**
+     * Creates a temp file
+     * @return resource
+     */
+    protected function createTempFile()
+    {
+        $tmpFile = tmpfile();
+        if (!is_resource($tmpFile)) {
+            throw FileException::cannotCreateTempFile();
+        }
+
+        return $tmpFile;
+    }
+
+    protected function closeTempFile($tmpFile): void
+    {
+        if (!is_resource($tmpFile)) {
+            return;
+        }
+        fclose($tmpFile);
+    }
+}
